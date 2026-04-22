08fee21cb19c701a168b5d17faf7467b69e88445
bomffas <oi@joaobomfim.com.br>
2026-04-21 22:46:50 -0300

    chore: remove default NestJS scaffold boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
@@ -20,7 +18,7 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
